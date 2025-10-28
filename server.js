de4f1c7fa2f22186d3eb26fb22db23fc104fffd6
smotoeffect-jpg <s.motoeffect@gmail.com>
2025-10-28 18:11:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "1mb" }));
+app.use(express.json({ limit: "1mb", type: "*/*" }));
 app.use(express.urlencoded({ extended: false }));
 
 // ====== CONFIG ======
